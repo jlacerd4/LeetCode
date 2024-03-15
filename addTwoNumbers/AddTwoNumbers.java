cfde6dd0cbd1d766997f1de58d0822b58c72b688
Geraldo de Lima Junior <geraldo.lima.junior@ccc.ufcg.edu.br>
2024-03-14 21:04:11 -0300

    fix: carries remainder when nodes are null

--- addTwoNumbers/AddTwoNumbers.java
+++ addTwoNumbers/AddTwoNumbers.java
@@ -12,19 +12,24 @@ class Solution {
     }
 
     private void addTwoNumbersAux(ListNode l1, ListNode l2, ListNode summingUpNode, int remainder){
-        if(l1 == null && l2 == null) return;
+        if(l1 == null && l2 == null) {
+            if (remainder > 0) {
+                summingUpNode.val = remainder;
+            }
+            return;
+        }
         if(l1 == null) {l1 = new ListNode(0);}
         if(l2 == null) {l2 = new ListNode(0);}
-
+    
         int sumVal = l1.val + l2.val + remainder;
         int correctedVal = sumVal % 10;
-        int newRemainder = (int) sumVal / 10;
-
-        ListNode newNode = new ListNode();
-
+        int newRemainder = sumVal / 10;
+    
         summingUpNode.val = correctedVal;
-        summingUpNode.next = newNode;
-
-        addTwoNumbersAux(l1.next, l2.next, newNode, newRemainder);
+    
+        if(l1.next != null || l2.next != null || newRemainder > 0) {
+            summingUpNode.next = new ListNode();
+            addTwoNumbersAux(l1.next, l2.next, summingUpNode.next, newRemainder);
+        }
     }
 }
